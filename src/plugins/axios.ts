090487a09492528538a3a19c22bdc465858d5396
Dev-SSS1267 <sams3s1267@gmail.com>
2024-11-06 11:34:14 +0900

    Update API base URLs and remove unnecessary path prefixes

--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api/v1',
+  baseURL: 'https://myuni-api.lunaiz.com/api/v1',
   headers: {
     'Content-Type': 'application/json',
     Authorization: `Bearer ${localStorage.getItem('token')}`,
